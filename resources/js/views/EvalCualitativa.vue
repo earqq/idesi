0cf92efb1e146843f647495457a249bcaaf95ca0
Elef Abner <erosalesq@gmail.com>
2020-02-06 10:34:44 -0500

    a

--- resources/js/views/EvalCualitativa.vue
+++ resources/js/views/EvalCualitativa.vue
@@ -500,7 +500,7 @@
                             </div>
                             <div class="input_wrapper">
                               <label>Telefono</label>
-                              <input v-model="referencia.telefono" type="text" />
+                              <input v-model="referencia.telefono" type="text" maxlength='11' />
                             </div>
 
                           </div>
